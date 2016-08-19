fb981d17a95cb8a6c0bb986b9dacb69fb1dd76ab
MORA <MORA@172.16.57.136>
2016-08-18 21:30:40 -0500

    First  commit

--- test/test/ProbadorRomanos.java
+++ test/test/ProbadorRomanos.java
@@ -3,7 +3,6 @@ package test;
 import java.io.FileNotFoundException;
 import java.util.Random;
 import org.testng.Assert;
-import static org.testng.Assert.*;
 import org.testng.annotations.Test;
 
 public class ProbadorRomanos {
@@ -34,5 +33,107 @@ public class ProbadorRomanos {
         int numero = 0;
         Assert.assertEquals(numero, 0);
     }
+    
+    //------------------------------------
+    
+    @Test
+    public void arrojaErrorAlRecibirUncero(){
+        Romano romano=FabricaRomanos.crearRomano();
+        Assert.assertFalse(romano.validaNumero(0));
+    }
+    @Test
+    public void arrojaErrorAlRecibirNumeroNegativo(){
+        Romano romano=FabricaRomanos.crearRomano();
+        Assert.assertFalse(romano.validaNumero(-1));
+    }
+
+    @Test
+    public void devuelveISiRecibeUn1(){
+        Romano romano=FabricaRomanos.crearRomano();
+        String valorEsperado="I";
+        Assert.assertTrue(valorEsperado.equalsIgnoreCase(romano.Convierte(1)));
+    }
+
+    @Test
+    public void devuelveVSiRecibeUn5(){
+        Romano romano=FabricaRomanos.crearRomano();
+        String valorEsperado="V";
+        Assert.assertTrue(valorEsperado.equalsIgnoreCase(romano.Convierte(5)));
+    }
+    @Test
+    public void devuelveXSiRecibeUn10(){
+        Romano romano=FabricaRomanos.crearRomano();
+        String valorEsperado="V";
+        Assert.assertTrue(valorEsperado.equalsIgnoreCase(romano.Convierte(10)));
+    }
+    @Test
+    public void devuelveLSiRecibeUn50(){
+        Romano romano=FabricaRomanos.crearRomano();
+        String valorEsperado="L";
+        Assert.assertTrue(valorEsperado.equalsIgnoreCase(romano.Convierte(50)));
+    }
+    @Test
+    public void devuelveCSiRecibeUn100(){
+        Romano romano=FabricaRomanos.crearRomano();
+        String valorEsperado="C";
+        Assert.assertTrue(valorEsperado.equalsIgnoreCase(romano.Convierte(100)));
+    }
+    @Test
+    public void devuelveDSiRecibeUn500(){
+        Romano romano=FabricaRomanos.crearRomano();
+        String valorEsperado="D";
+        Assert.assertTrue(valorEsperado.equalsIgnoreCase(romano.Convierte(500)));
+    }
+    @Test
+    public void devuelveMSiRecibeUn1000(){
+        Romano romano=FabricaRomanos.crearRomano();
+        String valorEsperado="M";
+        Assert.assertTrue(valorEsperado.equalsIgnoreCase(romano.Convierte(1000)));
+    }
+
+
+    @Test
+    public void devuelveIISiRecibeUn2(){
+        Romano romano=FabricaRomanos.crearRomano();
+        String valorEsperado="II";
+        Assert.assertTrue(valorEsperado.equalsIgnoreCase(romano.Convierte(2)));
+    }
+
+    @Test
+    public void devuelveIVSiRecibeUn4(){
+        Romano romano=FabricaRomanos.crearRomano();
+        String valorEsperado="IV";
+        Assert.assertTrue(valorEsperado.equalsIgnoreCase(romano.Convierte(4)));
+    }
+
+    @Test
+    public void devuelveIXSiRecibeUn9(){
+        Romano romano=FabricaRomanos.crearRomano();
+        String valorEsperado="IX";
+        Assert.assertTrue(valorEsperado.equalsIgnoreCase(romano.Convierte(9)));
+    }
+
+    @Test
+    public void devuelveCCCSiRecibeUn300(){
+        Romano romano=FabricaRomanos.crearRomano();
+        String valorEsperado="CCC";
+        Assert.assertTrue(valorEsperado.equalsIgnoreCase(romano.Convierte(300)));
+    }
+
+    @Test
+    public void devuelveCDSiRecibeUn400(){
+        Romano romano=FabricaRomanos.crearRomano();
+        String valorEsperado="CD";
+        Assert.assertTrue(valorEsperado.equalsIgnoreCase(romano.Convierte(400)));
+    }
+
+
+    public void toDo(){
+        revisaQueNoSeaUnaLetra
+                revisaQueNoSeaCaracterEspecial
+
+
+    }
        
 }
+
